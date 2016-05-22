5817511b7949d5179956042a71603fff33845c2f
Your Name <you@example.com>
2016-05-23 04:55:39 +0900

    my first commit

---
+++ app/src/main/java/com/example/kouhei/fragmentapp/second.java
@@ -0,0 +1,21 @@
+package com.example.kouhei.fragmentapp;
+
+/**
+ * Created by Kouhei on 16/05/02.
+ */
+public class second {
+
+    public static final String[] pin = {
+            "test01",
+            "test02",
+            "test03",
+            "test04"
+    };
+
+    public static final String[] po = {
+            "test11",
+            "test12",
+            "test13",
+            "test14"
+    };
+}
